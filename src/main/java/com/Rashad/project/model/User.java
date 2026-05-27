a1c9cd842ad2dbde6ba7c1deb05aebac1f04ed9d
Rashad Ibra <rashad.ibra2003@gmail.com>
2026-05-27 14:09:52 +0400

    commit

--- src/main/java/com/Rashad/project/model/User.java
+++ src/main/java/com/Rashad/project/model/User.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-    private String name;
-    private String email;
-    private String password;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
 }
